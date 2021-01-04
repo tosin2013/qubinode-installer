9ced43b05cc8ce06769958e7fe6a004a04fad219
Tosin Akinosho <tosin.akinosho@gmail.com>
2021-01-04 09:12:43 -0500

    adding update call to script

--- lib/qubinode_rpm_packages.sh
+++ lib/qubinode_rpm_packages.sh
@@ -13,6 +13,7 @@ function configure_centos8_packages(){
     sudo dnf -y install 'dnf-command(copr)'
     sudo dnf -y copr enable rhcontainerbot/container-selinux
     sudo curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_8/devel:kubic:libcontainers:stable.repo
+    sudo dnf update -y 
     sudo dnf -y install  buildah skopeo podman jq
 }
 
